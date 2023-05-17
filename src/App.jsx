0e39cf0faf84da97ff1e5ea5b1b1707b517839e7
Segun Osiki <osiki.segun@gmail.com>
2023-05-17 22:17:23 +0100

    create signup page

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Signup from "./pages/signup/Signup"
+
+
+
+function App() {
+
+
+  return (
+    <>
+      <Signup />
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/signup/Signup.jsx
@@ -0,0 +1,16 @@
+const Signup = () => {
+    return ( 
+        <section>
+           <section className="md:flex">
+            <section className="flex-1">
+                left
+            </section>
+            <section className="flex-1">
+                right
+            </section>
+           </section>
+        </section>
+     );
+}
+ 
+export default Signup;
