02ead96ce15c3c9054e61cce8f85b43b02424808
Segun Osiki <osiki.segun@gmail.com>
2023-05-28 15:31:12 +0100

    add table header

--- src/pages/books/BookList.jsx
+++ src/pages/books/BookList.jsx
@@ -13,14 +13,13 @@ const BookList = () => {
             <section className="flex shadow border-b md:ml-[-8px] ml-4">
                 <table className="min-w-full">
                     <thead className="bg-gray-50">
-                        <tr>
-                            <th className="text-left font-medium text-gray-500 uppercase tracking-wider py-3 px-8">TITLE</th>
-                            <th className="text-left font-medium text-gray-500 uppercase tracking-wider py-3 px-6">AUTHOR</th>
-                            <th className="text-left font-medium text-gray-500 uppercase tracking-wider py-3 px-6">PAGES</th>
-                            <th className="text-left font-medium text-gray-500 uppercase tracking-wider py-3 px-6">STATUS</th>
-                            <th className="text-right font-medium text-gray-500 uppercase tracking-wider py-3 px-6">ACTIONS</th>
+                        <tr className="flex justify-between">
+                            <th className="text-left text-xs font-medium md:text-base text-gray-500 uppercase md:tracking-wider py-3 md:px-6">TITLE</th>
+                            <th className="text-left text-xs font-medium md:text-base text-gray-500 uppercase md:tracking-wider py-3 md:px-6">AUTHOR</th>
+                            <th className="text-left text-xs font-medium md:text-base text-gray-500 uppercase md:tracking-wider py-3 md:px-6">PAGES</th>
+                            <th className="text-left text-xs font-medium md:text-base text-gray-500 uppercase md:tracking-wider py-3 md:px-6">STATUS</th>
+                            <th className="text-right text-xs font-medium md:text-base text-gray-500 uppercase md:tracking-wider py-3 md:px-6">ACTIONS</th>
                         </tr>
-
                     </thead>
 
                 </table>
