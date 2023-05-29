232b5f41d5835d565938c031d2447801676d2aca
Segun Osiki <osiki.segun@gmail.com>
2023-05-29 18:07:15 +0100

    create book service class

---
+++ src/services/CreateBookService.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+const CREATE_BOOK_URL = "http://localhost:8080/api/v1/book/create-book";
+
+class CreateBookService{
+    createBook(bookDto, id){
+        return axios.post(CREATE_BOOK_URL + "/" + id, bookDto);
+    }
+
+}
+
+
+
+export default new CreateBookService()
